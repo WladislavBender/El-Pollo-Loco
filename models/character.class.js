9498c1ae0a8b23338af379cf2a67522d4ddd9b44
Wladislav Bender <wladislav.bender@gmail.com>
2025-07-25 21:43:10 +0200

    added keyEventListener

--- models/character.class.js
+++ models/character.class.js
@@ -11,7 +11,8 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
     ];
-    
+    world;
+
 
 
     constructor() {
@@ -24,11 +25,13 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6 => 1, Rest 1
-            // i = 0, 1, 2, 3, 4, 5, 6, 0
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6 => 1, Rest 1
+                // i = 0, 1, 2, 3, 4, 5, 6, 0
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
         }, 100)
 
     }
