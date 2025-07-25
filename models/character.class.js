30f526b922f13ac9913ba2f525e18871954ceded
Wladislav Bender <wladislav.bender@gmail.com>
2025-07-25 18:01:47 +0200

    started with animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,25 @@
 class Character extends MovableObject {
 
-    constructor(){
-        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-    }
-       
-    
+    height = 300;
+    width = 150;
+    y = 135;
 
-    jump(){
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages([
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-22.png',
+            'img/2_character_pepe/2_walk/W-23.png',
+            'img/2_character_pepe/2_walk/W-24.png',
+            'img/2_character_pepe/2_walk/W-25.png',
+            'img/2_character_pepe/2_walk/W-26.png',
+        ])
+    }
+
+
+
+    jump() {
 
     }
 }
