d8edf73b23bced9e6b41494a8ccb71f6659bb8e4
Wladislav Bender <wladislav.bender@gmail.com>
2025-08-11 23:01:19 +0200

    added collectable objects

---
+++ models/collectable-object.class.js
@@ -0,0 +1,37 @@
+class CollectableObject extends MovableObject {
+    IMAGES_COLLECTABLE_BOTTLES = [
+        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
+    ];
+
+    IMAGES_COLLECTABLE_COINS = [
+        'img/8_coin/coin_1.png',
+        'img/8_coin/coin_2.png'
+    ];
+
+    type; // 'bottle' oder 'coin'
+
+    constructor(type, x, y) {
+        super();
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.height = 60;
+        this.width = 60;
+
+        if (type === 'bottle') {
+            let randomImg = this.IMAGES_COLLECTABLE_BOTTLES[Math.floor(Math.random() * this.IMAGES_COLLECTABLE_BOTTLES.length)];
+            this.loadImage(randomImg);
+        } else if (type === 'coin') {
+            this.loadImage(this.IMAGES_COLLECTABLE_COINS[0]);
+            this.loadImages(this.IMAGES_COLLECTABLE_COINS);
+            this.animateCoins();
+        }
+    }
+
+    animateCoins() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_COLLECTABLE_COINS);
+        }, 200);
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -7,16 +7,44 @@ class World {
     camera_x = 0;
     statusBar = new StatusBar();
     throwableObjects = [];
+    collectableObjects = [];
+    coins = 0;
+    bottles = 0;
+    totalCoins = 10; // für Prozentberechnung
+    totalBottles = 5;
 
 
 
+    // Im World-Konstruktor gleich initialisieren
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
+
+        this.bottles = 0; // WICHTIG: Startwert 0
+        this.coins = 0;
+        this.totalCoins = 10;
+        this.totalBottles = 5;
+
+        this.spawnCollectables();
         this.draw();
         this.setWorld();
         this.run();
+    }
+
+    spawnCollectables() {
+        // Zufällige Bottles
+        for (let i = 0; i < 5; i++) {
+            let x = Math.random() * 2000 + 200;
+            let y = 350;
+            this.collectableObjects.push(new CollectableObject('bottle', x, y));
+        }
+        // Zufällige Coins
+        for (let i = 0; i < 10; i++) {
+            let x = Math.random() * 2000 + 200;
+            let y = 300;
+            this.collectableObjects.push(new CollectableObject('coin', x, y));
+        }
     }
 
     setWorld() {
@@ -31,13 +59,34 @@ class World {
     }
 
     checkCollisions() {
+        // Gegner
         this.level.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
                 this.character.hit();
                 this.statusBar.setPercentage('health', this.character.energy);
             }
         });
+
+        // Collectables
+        this.collectableObjects = this.collectableObjects.filter(obj => {
+            if (this.character.isColliding(obj)) {
+                if (obj.type === 'coin') {
+                    this.coins++;
+                    let coinPercentage = Math.min((this.coins / 10) * 100, 100);
+                    this.statusBar.setPercentage('coins', coinPercentage);
+                } else if (obj.type === 'bottle') {
+                    if (this.bottles < 5) {
+                        this.bottles++;
+                        let bottlePercentage = (this.bottles / 5) * 100;
+                        this.statusBar.setPercentage('bottles', bottlePercentage);
+                    }
+                }
+                return false; // entfernt das eingesammelte Objekt
+            }
+            return true;
+        });
     }
+
 
     // Beispiel: wenn ein Coin eingesammelt wird
     collectCoin() {
@@ -52,13 +101,22 @@ class World {
         this.statusBar.setPercentage('bottles', bottlePercentage);
     }
 
-
     checkThrowObjects() {
-        if (this.keyboard.D) {
+        // Prüfen, ob Flaschen vorhanden sind und Taste nur einmal erkannt wird
+        if (this.keyboard.D && this.bottles > 0 && !this.throwCooldown) {
             let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
             this.throwableObjects.push(bottle);
+
+            this.bottles--;
+            let bottlePercentage = (this.bottles / 5) * 100;
+            this.statusBar.setPercentage('bottles', bottlePercentage);
+
+            // Kurzer Cooldown, damit nicht pro Frame eine geworfen wird
+            this.throwCooldown = true;
+            setTimeout(() => this.throwCooldown = false, 300);
         }
     }
+
 
 
     draw() {
@@ -76,6 +134,7 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.collectableObjects);
         this.addObjectsToMap(this.throwableObjects);
 
         this.ctx.translate(-this.camera_x, 0);
